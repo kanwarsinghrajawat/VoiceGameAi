2059c1670182785da01fcd766351cc9e185b0b82
Kanwar Singh <kanwarsinghrajawat3@gmail.com>
2023-11-24 15:04:45 +0530

    build-error

--- src/components/GridContext.tsx
+++ src/components/GridContext.tsx
@@ -1,15 +1,20 @@
 import { createContext, useState } from "react";
 export const GridContext = createContext({
   gridSize: { width: 16, height: 16 },
-  updateGridSize: (width: number, height: number) => {},
+  updateGridSize: (width: number, height: number) => {
+    console.log(width, height);
+  },
   speed: 500,
-  updateSpeed: (newSpeed: number) => {},
+  updateSpeed: (newSpeed: number) => {
+    console.log(newSpeed);
+  },
   spokenValue: "",
 });
 export const GridProvider = ({ children }: any) => {
   const [gridSize, setGridSize] = useState({ width: 16, height: 16 });
   const [speed, setSpeed] = useState(200); // Default speed value
   const [spokenValue, setSpokenValue] = useState("");
+  console.log(setSpokenValue);
 
   const updateGridSize = (width: number, height: number) => {
     setGridSize({ width, height });
